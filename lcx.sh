ed83c63ec7dd825b92df9f959ca8d0e766d2ed41
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-06-06 10:49:27 -0500

    Create lcx.sh

---
+++ lcx.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+sudo apt update
+
+sudo apt install -y build-essentials
+
+sudo apt install software-properties-common
+
+sudo apt install curl
+
+sudo apt install nano
+
+sudo apt install git
