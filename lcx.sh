39d34d181d25d763d89cadac4db39914fd06faaf
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-06-07 15:54:32 -0500

    Update lcx.sh

--- lcx.sh
+++ lcx.sh
@@ -2,15 +2,13 @@
 
 sudo apt update
 
-sudo apt install -y build-essentials
+sudo apt install -y build-essential
 
 sudo apt install software-properties-common
 
 sudo apt install curl
 
 sudo apt install nano
-
-sudo apt install git
 
 sudo apt install cifs-utils
 
