24399962990af2f65284db7ad2ed98837daead0d
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-10-01 02:04:07 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,11 +1,7 @@
 #!bin/bash
 
-sudo apt-get -y update
-sudo apt install -y curl
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker ${USER}
-sudo apt install docker-compose-plugin
-sudo apt upgrade -y --autoremove
-logout
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+exec bash
